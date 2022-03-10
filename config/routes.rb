832a2b822b4d2d8bc167c5ddeccfa79be48b5d20
kinocooper <95660939+kinocooper@users.noreply.github.com>
2022-03-10 14:08:44 +0000

    [add]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
-  rootto: 'homes#top', as: "root"
+  root to: 'homes#top', as: "root"
+  get "about" => "homes#about", as: "about"
+
+  resource :users, only:[:edit,:update]
+  get 'users/confirm' ,as: "u_confirm"
+  patch 'users/disabling' => "users#disabling", as: "disabling"
+
+  resource :pairs, only:[:edit,:update,:destroy] #create的なものはregistration#createでまとめる予定
+  get 'pairs/invite', as: "invite"
+  post "pairs/send", as: "send"
+  get 'pairs/complete', as: "complete"
+  get 'pairs/confirm', as: "p_confirm"
+
+  resources :discuss_records, only:[:new,:create,:show,:edhit,:index,:destroy] do
+    resources :personal_opinions, only:[:update]
+  end
+  patch "discuss_records/reconcile", as: "reconcile"
+  get 'discuss_records/congratulations', as: "congratulations"
+
+  resources :tags, only:[:new,:create,:destroy]
+
+  resources :tag_relationships, only:[:create,:destroy]
+
   get 'searches/search'
-  get 'discuss_records/new'
-  get 'discuss_records/show'
-  get 'discuss_records/edit'
-  get 'discuss_records/congratulations'
-  get 'discuss_records/index'
-  get 'pairs/edit'
-  get 'pairs/invite'
-  get 'pairs/complete'
-  get 'pairs/confirm'
-  get 'users/edit'
-  get 'users/confirm'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
