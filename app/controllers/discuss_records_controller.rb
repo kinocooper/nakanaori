4cfb1f7511111b7410ece19c89be2cdfc0ed3330
kinocooper <95660939+kinocooper@users.noreply.github.com>
2022-03-14 05:03:11 +0000

    [update]discuss_record投稿時にペア各々のpersonal_opinionを同時に生成 [add]投稿のフォームを部分テンプレート化

--- app/controllers/discuss_records_controller.rb
+++ app/controllers/discuss_records_controller.rb
@@ -1,20 +1,39 @@
 class DiscussRecordsController < ApplicationController
   def new
     @discuss_record = DiscussRecord.new
+    @my_opinion = PersonalOpinion.new
   end
 
   def create
+    # レコードの新規生成
     discuss_record = DiscussRecord.new(discuss_record_params)
-    discuss_record.save
-    redirect_to root_path, notice: "ケンカを投稿しました"
+    discuss_record.pair_id = current_user.pair_id
+    discuss_record.save #この時点で投稿の新規idが生成
+
+    # 結びつく自分の意見 新規生成
+    my_opinion = PersonalOpinion.new(personal_opinion_params[:personal_opinion])
+    my_opinion.discuss_record_id = discuss_record.id #ついさっき生成された投稿のid
+    my_opinion.user_id = current_user.id
+    my_opinion.save
+
+    # 結びつく相方の意見 新規生成(中身は空)
+    partners_opinion = PersonalOpinion.new
+    partners_opinion.discuss_record_id = discuss_record.id #ついさっき生成された投稿のid
+    partners_opinion.user_id = current_user.partner_id
+    partners_opinion.save
+
+    redirect_to discuss_record_path(discuss_record.id), notice: "ケンカを投稿しました"
   end
 
   def show
     @discuss_record = DiscussRecord.find(params[:id])
+    @my_opinion = @discuss_record.personal_opinions.find_by(user_id: current_user.id)
+    @partners_opinion = @discuss_record.personal_opinions.find_by(user_id: current_user.partner_id)
   end
 
   def edit
     @discuss_record = DiscussRecord.find(params[:id])
+    @my_opinion = @discuss_record.personal_opinions.find_by(user_id: current_user.id)
   end
 
   def update
@@ -40,7 +59,11 @@ class DiscussRecordsController < ApplicationController
   private
 
   def discuss_record_params
-    params.require(:discuss_record).permit(:pair_id,:title,:detail,:is_closed)
+    params.require(:discuss_record).permit(:title,:detail,:is_closed)
+  end
+
+  def personal_opinion_params
+    params.require(:discuss_record).permit(personal_opinion:[:claim,:conclude])
   end
 
 end
